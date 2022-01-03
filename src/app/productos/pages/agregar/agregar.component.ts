47852bf10a98aae97cd7f214208325539f5ec71f
Leandro <leandro.n.aguilera@gmail.com>
2022-01-03 17:38:32 -0300

    Input Setters, control de propiedades desde la directiva

--- src/app/productos/pages/agregar/agregar.component.ts
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -9,6 +9,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AgregarComponent  {
 
+  texto1: string = 'Leandrito';
+  color: string = 'red';
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', Validators.required ]
   })
@@ -17,6 +19,15 @@ export class AgregarComponent  {
 
   tieneError( campo: string ): boolean {
     return this.miFormulario.get(campo)?.invalid || false;
+  }
+
+  cambiarNombre() {
+    this.texto1 = "Roberto Carlos";
+  }
+
+  cambiarColor() {
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    this.color = color;
   }
 
 }
